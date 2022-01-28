8b46693e31e2ca30871fe8b45de5a8b9fcc97a6e
viktorShubin <shubin.vik97@gmail.com>
2022-01-29 01:36:28 +0800

    в сайдбаре поменял теги a на теги navlink чтоб при переходе по ссылке внутри проекта страница не перезагружалась

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,11 +1,12 @@
+import {NavLink} from "react-router-dom";
 
 const Sidebar = () => {
     return (
         <nav className="sidebar">
-            <div><a href="/profile">Profile</a> </div>
-            <div><a href="/message">Message</a></div>
-            <div><a href="/news">News</a></div>
-            <div><a href="/music">Music</a> </div>
+            <div><NavLink to="/profile">Profile</NavLink> </div>
+            <div><NavLink to="/message">Message</NavLink></div>
+            <div><NavLink to="/news">News</NavLink></div>
+            <div><NavLink to="/music">Music</NavLink> </div>
         </nav>
     )
 }
